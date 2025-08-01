72492730669387fb28c7ad8b3f0dca357b069a7b
fullstackdeveloper010124 <fullstackdeveloper010124@gmail.com>
2025-08-02 01:42:52 +0530

    changes server.js
    changes server.js

--- server.js
+++ server.js
@@ -43,6 +43,6 @@ app.get("/", (req, res) => {
 
 // Start Server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`🚀 Server running on http://localhost:${PORT}`);
+// });
