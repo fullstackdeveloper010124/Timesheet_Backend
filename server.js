862b1667666314d62d365c212dddc1d423e15c21
fullstackdeveloper010124 <fullstackdeveloper010124@gmail.com>
2025-08-05 00:54:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -21,8 +21,8 @@ const leaveRoutes = require("./routes/leaveApplication");
 
 // MongoDB Connection 
 mongoose.connect(process.env.MONGO_URI, {
-  // useNewUrlParser: true,
-  // useUnifiedTopology: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 })
 .then(() => console.log("✅ MongoDB connected"))
 .catch(err => console.error("❌ MongoDB connection error:", err));
